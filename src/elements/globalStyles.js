e67e6b53bb27d95d58dc4d45c87635bdfcfaa8ee
onisawa <onisawavisava@gmail.com>
2021-02-24 22:23:31 +0700

    add background color

--- src/elements/globalStyles.js
+++ src/elements/globalStyles.js
@@ -1,8 +1,10 @@
 import { createGlobalStyle }  from 'styled-components'
+import { BACKGROUND_COLOR } from '../constant'
 
 const GlobalStyles = createGlobalStyle`
 
   body {
+    background-color: ${BACKGROUND_COLOR};
     overflow-x: hidden;
     font-family: 'Poppins', sans-serif;
     margin: 0;
